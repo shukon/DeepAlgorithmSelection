FIX nan-bug, use scikit for normalization.

dlas/data_prep/data_prep.py
@@ -7,6 +7,7 @@ from dlas.data_prep.text_to_image import TextToImage
 from dlas.data_prep.from_image import FromImage
 
 from dlas.data_prep.multi_label_base import MultiLabelBase
+from sklearn.preprocessing import StandardScaler
 
 
 class DataPreparer(object):
@@ -55,10 +56,19 @@ class DataPreparer(object):
         self.log.debug("img_dir: {}, label_dir: {}".format(self.img_dir,
             self.label_dir))
 
-    def normalize(self, data):
+    def norm(self, data):
+        scale = StandardScaler() #with_mean=0, with_std=1)
+        #reshaped_data = data.reshape(data.shape[0], -1)
+        #print(reshaped_data.scale)
+        #scale.fit(reshaped_data)
+        #reshaped_data = scale.transform(reshaped_data)
+        #return data.reshape(data.shape)
+        return scale.fit_transform(data)
+
+        ####
         mean = np.mean(data, axis=0)
         std = np.std(data, axis=0)
-        if std.any == 0:
+        if (std==0).any():
             self.log.warning("ALERT! std = 0")
         data = (data-mean)/std
         return data
@@ -94,10 +104,24 @@ class DataPreparer(object):
 
         image_data = np.reshape(image_data, (-1, 1, self.config["image-dim"],
                                              self.config["image-dim"]))
+        num_inst = image_data.shape[0]
+        image_data = np.reshape(image_data, (num_inst, -1))
+        print(image_data.shape)
         image_data = self.float32(image_data)
-        image_data = self.normalize(image_data)
-        image_data = np.nan_to_num(image_data)
-        assert(np.isnan(image_data).any, False)
+        print("Before norm:")
+        print(image_data)
+        image_data = self.norm(image_data)
+        print("After norm:")
+        print(image_data)
+        image_data = np.reshape(image_data, (-1, 1, self.config["image-dim"],
+                                             self.config["image-dim"]))
+        if ((image_data==0).all()):
+            self.log.error("Something went wrong with preprocessing(< -1)")
+        if np.isnan(image_data).any():
+            self.log.error("Something went wrong with preprocessing image-data")
+        #image_data = np.nan_to_num(image_data)
+        #assert np.isnan(image_data).any(), False
+        #print(image_data)
 
         self.log.debug("Saving image-data in {}".format(path))
         np.save(path, image_data)
@@ -115,7 +139,6 @@ class DataPreparer(object):
             if self.label_output, writes data into output_dir
         """
         path = os.path.join(self.label_dir, self.label_prep.id+".npy")
-        self.log.debug("Checking for label-data in {}".format(path))
         if (not recalculate and self.label_dir and os.path.isfile(path)):
             self.log.debug("Loading label-data from {}".format(path))
             return np.load(path)

dlas/neural_net/network.py
@@ -285,9 +285,9 @@ class Network:
         self.input_var, self.target_var = T.tensor4("inputs"), T.matrix("targets")
 
         # Build network
-        if   self.config["model"] == "cnn": network = self.build_cnn(self.input_var)
-        elif self.config["model"] == "mnn": network = self.build_mnn(self.input_var)
-        elif self.config["model"] == "cnn1D":
+        if   self.config["nn-model"] == "cnn": network = self.build_cnn(self.input_var)
+        elif self.config["nn-model"] == "mnn": network = self.build_mnn(self.input_var)
+        elif self.config["nn-model"] == "cnn1D":
             self.input_var = T.tensor3("inputs")
             network = self.build_cnn1D(self.input_var)
         else: raise ValueError("{} not a modelchoice!".format(self.config["model"]))
@@ -373,7 +373,7 @@ class Network:
 
         useValidation = self.config["useValidationSet"]
 
-        if self.config["model"] == "cnn1D":
+        if self.config["nn-model"] == "cnn1D":
             X = X.reshape(-1,1,self.config["imageDim"]*self.config["imageDim"])
             X_val = X_val.reshape(-1,1,self.config["imageDim"]*self.config["imageDim"])
             X_test = X_test.reshape(-1,1,self.config["imageDim"]*self.config["imageDim"])
@@ -461,10 +461,10 @@ class Network:
             log.info(res)
 
         # Saving the network weights
-        if save:
-            path = self.config["modelPath"].format(scen, cv[0], cv[1], self.config["repetition"])
-            log.info("Save network in {}".format(path))
-            np.savez(path, *lasagne.layers.get_all_param_values(self.network))
+        #if save:
+        #    path = self.config["modelPath"].format(scen, cv[0], cv[1], self.config["repetition"])
+        #    log.info("Save network in {}".format(path))
+        #    np.savez(path, *lasagne.layers.get_all_param_values(self.network))
 
         return timesPerEpoch, timesToPredict, trainloss, valloss, testloss, trainpred, valpred, testpred, valAcc, testAcc
 